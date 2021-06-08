9c163403027e3d0d100be26530c1f0591613b31b
Thomas Lefebvre <thomas@unsplash.com>
2021-06-07 21:04:03 -0400

    WIP

---
+++ _extension/sample-app/index.ts
@@ -0,0 +1,4 @@
+const a = () => {};
+
+// This property should not be suggested by TS in the completion list
+a.caller;
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,38 @@
 import * as ts_module from "typescript/lib/tsserverlibrary";
 
-function init(modules: {typescript: typeof ts_module}) {
-    const ts = modules.typescript;
+function init(modules: { typescript: typeof ts_module }) {
+  const ts = modules.typescript;
 
-    function create(info: ts.server.PluginCreateInfo) {
-        // Get a list of things to remove from the completion list from the config object.
-        // If nothing was specified, we'll just remove 'caller'
-        const whatToRemove: string[] = info.config.remove || ['caller'];
+  function create(info: ts.server.PluginCreateInfo) {
+    // Diagnostic logging
+    info.project.projectService.logger.info(
+      "This message will appear in your logfile if the plugin loaded correctly"
+    );
 
-        // Diagnostic logging
-        info.project.projectService.logger.info("This message will appear in your logfile if the plugin loaded correctly");
-
-        // Set up decorator
-   	    const proxy = Object.create(null) as ts.LanguageService;
-	    const oldLS = info.languageService;
-	    for (const k in oldLS) {
-	        (<any>proxy)[k] = function () {
-	            return oldLS[k].apply(oldLS, arguments);
-	        }
-	    }
-
-        // Remove specified entries from completion list
-        proxy.getCompletionsAtPosition = (fileName, position) => {
-            const prior = info.languageService.getCompletionsAtPosition(fileName, position, undefined);
-            const oldLength = prior.entries.length;
-            prior.entries = prior.entries.filter(e => whatToRemove.indexOf(e.name) < 0);
-
-            // Sample logging for diagnostic purposes
-            if (oldLength !== prior.entries.length) {
-                info.project.projectService.logger.info(`Removed ${oldLength - prior.entries.length} entries from the completion list`);
-            }
-
-            return prior;
-        };
-
-        return proxy;
+    // Set up decorator
+    const proxy = Object.create(null) as ts.LanguageService;
+    const oldLS = info.languageService;
+    for (const k in oldLS) {
+      (<any>proxy)[k] = function () {
+        return oldLS[k].apply(oldLS, arguments);
+      };
     }
 
-    return { create };
+    // Remove specified entries from completion list
+    proxy.getCompletionsAtPosition = (fileName, position, options) => {
+      const prior = info.languageService.getCompletionsAtPosition(
+        fileName,
+        position,
+        options
+      );
+      prior.entries = prior.entries.filter((e) => e.name !== "caller");
+      return prior;
+    };
+
+    return proxy;
+  }
+
+  return { create };
 }
 
 export = init;
