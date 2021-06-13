5494499a2280003ff695b41570803b9be93db934
Thomas Lefebvre <thomas@unsplash.com>
2021-06-13 15:56:30 -0400

    Sample ts plugin refactor

--- _extension/sample-app/index.ts
+++ _extension/sample-app/index.ts
@@ -2,3 +2,5 @@ const a = () => {};
 
 // This property should not be suggested by TS in the completion list
 a.caller;
+
+class Test {}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,44 @@
 import * as ts_module from "typescript/lib/tsserverlibrary";
 
+const pluginName = "ts-refactor-tools";
+
 function init(modules: { typescript: typeof ts_module }) {
   const ts = modules.typescript;
 
-  function create(info: ts.server.PluginCreateInfo) {
-    // Diagnostic logging
-    info.project.projectService.logger.info(
-      "This message will appear in your logfile if the plugin loaded correctly"
-    );
+  function positionOrRangeToRange(
+    positionOrRange: number | ts_module.TextRange
+  ): ts_module.TextRange {
+    return typeof positionOrRange === "number"
+      ? { pos: positionOrRange, end: positionOrRange }
+      : positionOrRange;
+  }
 
+  /**normalize the parameter so we are sure is of type number */
+  function positionOrRangeToNumber(
+    positionOrRange: number | ts_module.TextRange
+  ): number {
+    return typeof positionOrRange === "number"
+      ? positionOrRange
+      : (positionOrRange as ts_module.TextRange).pos;
+  }
+
+  /** from given position we find the child node that contains it */
+  function findChildContainingPosition(
+    sourceFile: ts.SourceFile,
+    position: number
+  ): ts.Node | undefined {
+    function find(node: ts.Node): ts.Node | undefined {
+      if (position >= node.getStart() && position < node.getEnd()) {
+        return ts.forEachChild(node, find) || node;
+      }
+    }
+
+    return find(sourceFile);
+  }
+
+  function create(info: ts.server.PluginCreateInfo) {
+    const log = (msg: any) =>
+      info.project.projectService.logger.info(`[${pluginName}] ${msg}`);
     // Set up decorator
     const proxy = Object.create(null) as ts.LanguageService;
     const oldLS = info.languageService;
@@ -18,16 +48,106 @@ function init(modules: { typescript: typeof ts_module }) {
       };
     }
 
-    // Remove specified entries from completion list
-    proxy.getCompletionsAtPosition = (fileName, position, options) => {
-      const prior = info.languageService.getCompletionsAtPosition(
-        fileName,
-        position,
-        options
+    proxy.getApplicableRefactors = (
+      filename,
+      positionOrRange
+    ): ts_module.ApplicableRefactorInfo[] => {
+      const refactors = info.languageService.getApplicableRefactors(
+        filename,
+        positionOrRange,
+        undefined
       );
-      prior.entries = prior.entries.filter((e) => e.name !== "toString");
 
-      return prior;
+      const sourceFile = info.languageService
+        .getProgram()
+        .getSourceFile(filename);
+
+      const nodeAtCursor = findChildContainingPosition(
+        sourceFile,
+        positionOrRangeToNumber(positionOrRange)
+      );
+
+      log(nodeAtCursor.kind);
+
+      if (
+        nodeAtCursor !== undefined &&
+        nodeAtCursor.kind === ts.SyntaxKind.Identifier &&
+        nodeAtCursor.parent &&
+        [
+          ts.SyntaxKind.InterfaceDeclaration,
+          ts.SyntaxKind.ClassDeclaration,
+        ].includes(nodeAtCursor.parent.kind)
+      ) {
+        const refactorInfo: ts_module.ApplicableRefactorInfo = {
+          name: "useless-rename-info",
+          description: "useless rename desc",
+          actions: [{ name: "useless-rename", description: "Useless Rename" }],
+        };
+        refactors.push(refactorInfo);
+        return refactors;
+      } else {
+        return refactors;
+      }
+    };
+
+    proxy.getEditsForRefactor = (
+      fileName,
+      formatOptions,
+      positionOrRange,
+      refactorName,
+      actionName,
+      preferences
+    ) => {
+      const refactors = info.languageService.getEditsForRefactor(
+        fileName,
+        formatOptions,
+        positionOrRange,
+        refactorName,
+        actionName,
+        preferences
+      );
+
+      log("Execute " + actionName)
+
+      if (actionName !== "useless-rename") {
+        return refactors;
+      }
+
+      const sourceFile = info.languageService
+        .getProgram()
+        .getSourceFile(fileName);
+      const nodeAtCursor = findChildContainingPosition(
+        sourceFile,
+        positionOrRangeToNumber(positionOrRange)
+      );
+
+      log(`edits: ${nodeAtCursor.kind}`)
+
+      if (
+        nodeAtCursor !== undefined &&
+        nodeAtCursor.kind === ts.SyntaxKind.Identifier
+      ) {
+        const renameTo =
+          "Beautiful" + (nodeAtCursor as ts.Identifier).escapedText;
+        const range = positionOrRangeToRange(positionOrRange);
+        return {
+          edits: [
+            {
+              fileName,
+              textChanges: [
+                {
+                  span: { start: range.pos, length: range.end - range.pos }, // the segment of code that will be replaced
+                  newText: renameTo,
+                },
+              ],
+            },
+          ],
+          renameFilename: undefined,
+          renameLocation: undefined,
+        };
+      } else {
+        return refactors;
+      }
     };
 
     return proxy;
@@ -35,5 +155,7 @@ function init(modules: { typescript: typeof ts_module }) {
 
   return { create };
 }
+
+/**normalize the parameter so we are sure is of type Range */
 
 export = init;
